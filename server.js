4c0a4ddc8cc837410d45d61968d41f4e7910e798
Alejandro Lopez <reykrad@gmail.com>
2017-12-11 11:53:44 -0600

    Segunda parte del tutorial

--- server.js
+++ server.js
@@ -5,11 +5,6 @@ function iniciar(route, handle) {
 	function onRequest(req, res) {
 		var pathname = url.parse(req.url).pathname;
 		console.log("Peticion para " + pathname + " recibida.");
-		
-		//res.writeHead(200, {"Content-Type": "text/html"});
-		//var content = route(handle, pathname);
-		//res.write(content);
-		//res.end();
 		route(handle, pathname, res);
 	}
 
